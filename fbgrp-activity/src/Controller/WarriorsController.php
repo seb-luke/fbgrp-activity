805c72605e2d25f57f26cc2d7d421962cadc7a50
Sebastian Luca <seb.luke@gmail.com>
2018-01-07 21:07:00 +0200

    Created controllers to add and manage a group

--- fbgrp-activity/src/Controller/WarriorsController.php
+++ fbgrp-activity/src/Controller/WarriorsController.php
@@ -2,14 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\FacebookGroups;
 use App\Services\FacebookApiService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * Class WarriorsController
+ * @package App\Controller
+ * @Route("/warriors")
+ */
 class WarriorsController extends Controller
 {
     /**
-     * @Route("/warriors", name="warriorsIndex")
+     * @Route("/", name="warriorsIndex")
      * @param FacebookApiService $fbService
      * @return \Symfony\Component\HttpFoundation\Response
      */
@@ -18,9 +24,42 @@ class WarriorsController extends Controller
         $loggedUser = $this->getUser();
         $groups = $fbService->getGroupsWhereUserIsAdmin($loggedUser);
 
-        return $this->render('App/Warriors/warriorsIndex.html.twig', [
+        $groupRepo = $this->getDoctrine()->getManager()->getRepository(FacebookGroups::class);
+        foreach ($groups as &$grp) {
+
+            $isManaged = false;
+            $fbGroup = $groupRepo->find($grp["id"]);
+            if ($fbGroup != null) {
+                $isManaged = true;
+            }
+
+            $grp['isWarriorManaged'] = $isManaged;
+        }
+
+        dump($groups);
+        return $this->render('App/Warriors/index.html.twig', [
             'user' => ['name' => $loggedUser->getName(), 'surname' => $loggedUser->getSurname()],
             'groups' => $groups
         ]);
+    }
+
+    /**
+     * @Route("/group/add/{grpId}", name="warriorsAddGroup")
+     * @param $grpId
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function warriorsAddGroup($grpId)
+    {
+        return $this->render('App/Warriors/addGroup.html.twig');
+    }
+
+    /**
+     * @Route("/group/manage/{grpId}", name="warriorsManageGroup")
+     * @param $grpId
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function warriorsManageGroup($grpId)
+    {
+        return $this->render('App/Warriors/manageGroup.html.twig');
     }
 }
